ca2501290c8f81ae9262ff30aa921c908a118086
bluelikemilk <bluelikemilk@gmail.com>
2019-08-01 10:39:40 -0400

    Add addHexagon() in HexWorld.java in Lab5

--- proj2/byog/lab5/HexWorld.java
+++ proj2/byog/lab5/HexWorld.java
@@ -12,4 +12,27 @@ import java.util.Random;
  * Draws a world consisting of hexagonal regions.
  */
 public class HexWorld {
+    private static final int WIDTH = 20;
+    private static final int HEIGHT = 20;
+
+    public static void addHexagon(TETile[][] tiles, int i, int j, int s) {
+        int width = 3 * s - 2;
+        int height = 2 * s;
+        // left bottom corner of the hex is (i,j)
+        for (int x = i; x < i + width; x += 1) {
+            for (int y = j; y < j + height; y += 1) {
+                tiles[x][y] = Tileset.TREE;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        TERenderer ter = new TERenderer();
+        ter.initialize(WIDTH, HEIGHT);
+
+        TETile[][] hexWorld = new TETile[WIDTH][HEIGHT];
+        addHexagon(hexWorld, 2, 2, 3);
+
+        ter.renderFrame(hexWorld);
+    }
 }
